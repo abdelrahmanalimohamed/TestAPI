56e889e18bd71bcc8e2c55994617946ca0075828
abdelrahmanali <abdelrahmanali.mohamed@superhog.com>
2024-09-29 12:15:38 +0300

    test the pipeline

--- TestAPI/Controllers/WeatherForecastController.cs
+++ TestAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Abdo"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
